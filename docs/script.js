7b6a01c928a07237d11a4f13490787ed21824ed0
LOKESH S <lokeshsatheeshkumar@gmail.com>
2025-08-13 12:38:31 +0530

    Add files via upload

---
+++ docs/script.js
@@ -0,0 +1,14 @@
+// function bulbon(){
+//   document.getElementById("image").src = "";
+// }
+// function bulboff(){
+//   document.getElementById("image").src = "";
+// }
+
+
+function bulboff(){
+    document.getElementById("image").src="bulboff.jpg";
+}
+function bulbon(){
+    document.getElementById("image").src="bulbglow.jpg";
+}
